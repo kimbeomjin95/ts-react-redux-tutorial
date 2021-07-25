dae1a2a7ad9ed141a9f28d2a22e0c651494a5024
kimbeomjin95 <kimbeomjin95@github.com>
2021-07-25 17:59:37 +0900

    git ignore yarn.lock 추가

--- src/modules/github/reducer.ts
+++ src/modules/github/reducer.ts
@@ -1,45 +1,34 @@
-import { createReducer } from 'typesafe-actions';
+import {createReducer} from 'typesafe-actions';
 import {
   GET_USER_PROFILE,
   GET_USER_PROFILE_ERROR,
-  GET_USER_PROFILE_SUCCESS,
+  GET_USER_PROFILE_SUCCESS, getUserProfileAsync,
 } from './actions';
-import { GithubAction, GithubState } from './types';
+import {GithubAction, GithubState} from './types';
+import {asyncState, createAsyncReducer, transformToArray} from "../../lib/reducerUtils";
 
 const initialState: GithubState = {
-  userProfile: {
-    loading: false,
-    error: null,
-    data: null,
-  },
+  userProfile: asyncState.initial()
 };
 
-const github = createReducer<GithubState, GithubAction>(initialState, {
-  // 상태 업데이트
-  [GET_USER_PROFILE]: state => ({
-    ...state,
-    userProfile: {
-      loading: true,
-      error: null,
-      data: null,
-    },
-  }),
-  [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
-    ...state,
-    userProfile: {
-      loading: false,
-      error: null,
-      data: action.payload,
-    },
-  }),
-  [GET_USER_PROFILE_ERROR]: (state, action) => ({
-    ...state,
-    userProfile: {
-      loading: false,
-      error: action.payload,
-      data: null,
-    },
-  }),
-});
+const github = createReducer<GithubState, GithubAction>(
+  initialState)
+  // @ts-ignore
+  .handleAction(transformToArray(getUserProfileAsync), createAsyncReducer(getUserProfileAsync, 'userProfile'));
+// 상태 업데이트
+//   [GET_USER_PROFILE]: state => ({
+//     ...state,
+//     userProfile: asyncState.load() // 만약 로딩을 할 때 기존 데이터를 유지하고 싶다면 asyncState.load(state.userProfile.data)
+//   }),
+//   [GET_USER_PROFILE_SUCCESS]: (state, action) => ({f
+//     ...state,
+//     userProfile: asyncState.success(action.payload)
+//   }),l
+// //   [GET_USER_PROFILE_ERROR]: (state, action) => ({
+// //     ...state,
+// //     userProfile: asyncState.error(action.payoad)
+//   }),
+// });
+
 
 export default github;
--- src/modules/github/thunks.ts
+++ src/modules/github/thunks.ts
@@ -1,6 +1,6 @@
-import { Dispatch } from 'redux';
 import { getUserProfile, GithubProfile } from '../../api/github';
 import { getUserProfileAsync } from './actions';
+import createAsyncThunk from "../../lib/createAsyncThunk";
 
 // ThunkAction<R, S, E, A>
 // R: return type(thunk함수에서 리턴하는 타입)
@@ -10,15 +10,18 @@ import { getUserProfileAsync } from './actions';
 // export function getUserProfileThunk(username: string): ThunkAction<void, RootState, null, GithubProfile> { // getUserProfileThunk의 결과값이 ThunkAction
 
 // velopert 선호(thunk 함수)
-export function getUserProfileThunk(username: string) {
-  return async (dispatch: Dispatch) => {
-    const { request, success, failure } = getUserProfileAsync;
-    dispatch(request());
-    try {
-      const userProfile = await getUserProfile(username);
-      dispatch(success(userProfile));
-    } catch (e) {
-      dispatch(failure(e));
-    }
-  };
-}
+// export function getUserProfileThunk(username: string) {
+//   return async (dispatch: Dispatch) => {
+//     const { request, success, failure } = getUserProfileAsync;
+//     dispatch(request());
+//     try {
+//       const userProfile = await getUserProfile(username);
+//       dispatch(success(userProfile));
+//     } catch (e) {
+//       dispatch(failure(e));
+//     }
+//   };
+// }
+
+// @ts-ignore
+export const getUserProfileThunk = createAsyncThunk(getUserProfileAsync, getUserProfile);
